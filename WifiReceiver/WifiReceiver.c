08c406227449edada953b42d6268537306fe6045
Emmanuel Proulx <foufoulefou2juillet@hotmail.com>
2018-04-24 15:23:46 -0400

    Final comments and cleanup

--- WifiReceiver/WifiReceiver.c
+++ WifiReceiver/WifiReceiver.c
@@ -5,65 +5,53 @@
  *  Author: Equipe 24
  */ 
 
+/* ============ Include all required libraries ============= */
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-
 #include "driver.h"
 #include "uart.h"
 #include "fifo.h"
 #include "lcd.h"
 
-#define WAIT_DATA 0 // Attente d'une transmission
-#define WAIT_OPEN_BRACKET 1 // Début de la transmission
-#define READ_DATA 2 // Lecture de la transmission
-#define DISPLAY_DATA 3 // Affichage
+/* =========== Define the reception states ============= */
+#define WAIT_OPEN_BRACKET 1 // Wait for beginning of data transmission
+#define READ_DATA 2 // Read data transmission
+#define PROCESS_DATA 3 // Data processing
 
-bool isOnBoard;
+
+/* ==================== AIRBOAT VARIABLES ==================== */
+bool firstLoop;
+bool cutEngines;
+bool emergencyBrake;
+char aText;
+char aHorizontal[4];
+char aVertical[4];
+char aFlyMode;
+uint8_t aNbChar;
+uint8_t aHorizontalValue;
+uint8_t aVerticalValue;
+uint8_t aReceptionState;
+uint8_t mBatterie;
+uint8_t loopCount;
+uint8_t tempStoredLift;
+
+/* =================== All methods used by the microcontroller ============== */
 uint8_t getMaxBatteryValue(uint8_t);
 uint8_t getBatteryUsagePercentage(uint8_t,uint8_t,uint8_t);
 uint8_t getRealBatteryTension(uint8_t,uint8_t);
-void DelBattery(uint8_t);
-/* ==================== AIRBOAT VARIABLES ==================== */
-char aText;
-uint8_t aNbChar;
-char aHorizontale[4];
-char aVerticale[4];
-char aFlyMode;
-uint8_t aValeurHorizontale;
-uint8_t aValeurVerticale;
-uint8_t aEtatReception;
 void initializeAirboat();
 void waitForOpenBrackets();
 void readData();
-void displayData();
-bool firstLoop;
-uint8_t mBatterie;
-uint8_t loopCount;
-bool cutEngines;
-bool emergencyBrake;
-uint8_t tempStoredLift;
+void processData();
+void batteryProtection();
 
 int main(void)
 {
-	/* ================== INITIALISATION GLOBALE ================== */
-	lcd_init();
-	adc_init();
-	pwm_init(1,1);
-	uart_init();
-	uart_clean_rx_buffer();
-	servo_init();
-	SREG = set_bit(SREG, 7);
-	uart_set_baudrate(BAUDRATE_9600);
-	
-	/* Lecture de la valeur déterminant le type de contrôlleur (remote/receiver) */
-	
-	
-	
-	/* Effectuer le code de l'aéroglisseur */
-
+	/* ================ Initialize all the values and microcontroller ports on the ATMEGA32 ================ */
 	initializeAirboat();
-		
+	
+	/* ================ Microcontroller loop ================ */
 	while(1){
 		/* ================== ASSIGNATION DES MOTEURS ================== */
 		if(!cutEngines){
@@ -71,14 +59,14 @@ int main(void)
 				pwm_set_a(0);
 			}else{
 				if (aFlyMode == '1') {
-					pwm_set_a(aValeurVerticale);
+					pwm_set_a(aVerticalValue);
 					pwm_set_b(0);
-					tempStoredLift = aValeurVerticale;
+					tempStoredLift = aVerticalValue;
 				}
 				else if (aFlyMode == '0') {
-					pwm_set_b(aValeurVerticale);
-					if(aValeurHorizontale < 64 || aValeurHorizontale > 192){
-						pwm_set_a(42);
+					pwm_set_b(aVerticalValue);
+					if(aHorizontalValue < 64 || aHorizontalValue > 192){
+						pwm_set_a(55);
 					}else{
 						pwm_set_a(tempStoredLift);
 					}
@@ -87,76 +75,64 @@ int main(void)
 				}
 			}
 			
-			servo_set_a(aValeurHorizontale);
+			servo_set_a(aHorizontalValue);
 		}
 		
-		/* ================== PROGRAMME PRINCIPAL ================== */
-			// Si le buffer de réception n'est plus vide
+		/* ================== DATA RECEPTION ================== */
+			// If the reception buffer is not empty (we have received data)
 		if(!uart_is_rx_buffer_empty() && firstLoop != 1) {
+			//Get the byte from UART
 			aText = uart_get_byte();
-			switch (aEtatReception) {
-				/* ================== Attente d'entrée de données ================== */
+			switch (aReceptionState) {
+				/* ================== Wait for start of data stream ================== */
 				case WAIT_OPEN_BRACKET:waitForOpenBrackets();break;
 				
-				/* ================== Lecture des données ==================== */
+				/* ================== Read data ==================== */
 				case READ_DATA: readData();break;
 				
-				/* ================== Affichage des données sur le LCD ================== */
-				case DISPLAY_DATA: displayData();break;
+				/* ================== Process data ================== */
+				case PROCESS_DATA: processData();break;
 				
-				/* ================== Affichage d'une erreur en cas d'absence d'état ================== */
-				default:lcd_write_string("Error in machine state");break;
+				/* ================== In case of an error, just break ================== */
+				default:break;
 			}
 			
 		
 		}
-		firstLoop = 0;
-		mBatterie = getRealBatteryTension(adc_read(PA3), 9);
-		if(mBatterie < 63){
-			if(mBatterie < 60){
-				cutEngines = 1;
-				if(loopCount < 100){
-					PORTB = set_bit(PORTB, 4);
-				}else{
-					PORTB = clear_bit(PORTB, 4);
-					if(loopCount == 600){
-						loopCount = 0;
-					}
-				}
-				loopCount++;
-			}
-			else{
-				cutEngines = 0;
-				PORTB = set_bit(PORTB,4);
-			}
-			
-		}
+		
+		/* ========== Protect the battery from undervoltage (cut the engines at 6V) ============= */
+		batteryProtection();
 	
+		firstLoop = 0;
 	}
 	
 	
 	
 }
 	
-
-
-/* ================== MÉTHODES POUR L'AÉROGLISSEUR ================== */
+/* ========================= ALL METHODS =========================== */
 
 /************************************************************************/
-/* Initialiser l'aéroglisseur                                           */
+/* Initialize the airboat microcontroller                               */
 /************************************************************************/
 void initializeAirboat(){
+	lcd_init();
+	adc_init();
+	pwm_init(1,1);
+	uart_init();
 	uart_clean_rx_buffer();
-	OSCCAL = OSCCAL + 6; // Calibration de la fréquence du uC de l'aéroglisseur
-	DDRD = set_bit(DDRD, PD2); // pin 16 (PD2) = true (RST de l'ESP)
+	servo_init();
+	SREG = set_bit(SREG, 7);
+	uart_set_baudrate(BAUDRATE_9600);
+	OSCCAL = OSCCAL + 6; // Frequency calibration of the microcontroller
+	DDRD = set_bit(DDRD, PD2);
 	PORTD = clear_bit(PORTD, PD2);
 	_delay_ms(1500);
 	PORTD = set_bit(PORTD, PD2);
 	_delay_ms(1000);
-	uart_put_string("AT+CIPMODE=1\r\n\0"); // Mode Passthrough
+	uart_put_string("AT+CIPMODE=1\r\n\0"); // Passthrough mode
 	_delay_ms(2500);
-	uart_put_string("AT+CIPSTART=\"UDP\",\"0.0.0.0\",123,456\r\n\0"); // Établir la transmission UDP
-	
+	uart_put_string("AT+CIPSTART=\"UDP\",\"0.0.0.0\",123,456\r\n\0"); // Set the UDP transmission
 	lcd_clear_display();
 	lcd_write_string("Connected. :)");
 	_delay_ms(1000);
@@ -168,78 +144,75 @@ void initializeAirboat(){
 	DDRB = set_bit(DDRB,PB4);
 	aNbChar = 0;
 	aFlyMode = '0';
-	aValeurHorizontale = 128;
-	aValeurVerticale = 0;
+	aHorizontalValue = 128;
+	aVerticalValue = 0;
 	firstLoop = 1;
 	loopCount = 0;
 	cutEngines = 0;
-	aEtatReception = WAIT_OPEN_BRACKET;
+	aReceptionState = WAIT_OPEN_BRACKET;
 	PORTB = set_bit(PORTB, 0);
 	emergencyBrake = 0;
 	tempStoredLift = 0;
 }	
 
 /************************************************************************/
-/* Attente d'un segment de données                                      */
+/* Wait a data packet                                                   */
 /************************************************************************/
 void waitForOpenBrackets(){
 	if (aText == '[') {
 		PORTB = set_bit(PORTB, 1);
-		aEtatReception = READ_DATA;
+		aReceptionState = READ_DATA;
 		aNbChar = 0;
 	}
 } 
 
 /************************************************************************/
-/* Lire les données envoyées par la télécommande                        */
+/* Read data received from the controller                               */
 /************************************************************************/
 void readData(){
-	/* Tant qu'on atteint pas la fin du segment de données... */
+	/* While the data stream does not close... */
 	if (aText != ']'){
-		/* 3 premiers caractères: valeur de direction horizontale */
+		/* The first 3 characters are the horizontal value to set */
 		if (aNbChar < 3){
-			aHorizontale[aNbChar] = aText;	
-		/* Caractères 3 à 5: valeur de direction verticale */
+			aHorizontal[aNbChar] = aText;	
+		/* Characters 3 to 5: the vertical value */
 		}else if (aNbChar >= 3 && aNbChar < 6){
-			aVerticale[aNbChar - 3] = aText;
-		/* Dernier caractère: Mode de pilotage */
+			aVertical[aNbChar - 3] = aText;
+		/* The last character define the fly mode */
 		}else if (aNbChar >= 6){
-			/* Mode de lancement */
+			/* Detect an emergency brake */
 			if(aText == 'B'){
 				emergencyBrake = 1;
 			}else{
 				emergencyBrake = 0;
 				if (aText == 'L'){
 					aFlyMode = '1'; // LIFT MODE
-					/* Mode vitesse */
 					}else{
 					aFlyMode = '0'; // SPEED MODE
 				}
 			}
 			
 		}
-		/* Itérer la position dans le segment de données */
+		/* Increment the index in the data stream */
 		aNbChar++;
 	}
 	else {
-		/* Réinitialiser la position dans le segment et envoyer les données au LCD */
+		/* Reinitialize the index when the data stream is closed*/
 		aNbChar = 0;
-		aEtatReception = DISPLAY_DATA;
+		aReceptionState = PROCESS_DATA;
 	}
 }
 
 /************************************************************************/
-/* Afficher les valeurs reçues sur l'écran                              */
+/* Process the data received from the controller                        */
 /************************************************************************/
-void displayData(){
-	// Initialisation du lcd
-	aHorizontale[3] = 0;
-	aVerticale[3] = 0;
+void processData(){
+	aHorizontal[3] = 0;
+	aVertical[3] = 0;
 	uart_clean_rx_buffer();	
-	//_delay_ms(100);
-	aEtatReception = WAIT_OPEN_BRACKET;
-	aValeurHorizontale = string_to_uint(aHorizontale);
-	aValeurVerticale = string_to_uint(aVerticale);
+	aReceptionState = WAIT_OPEN_BRACKET;
+	aHorizontalValue = string_to_uint(aHorizontal);
+	aVerticalValue = string_to_uint(aVertical);
 }
 
 /**************************************************************************/
@@ -250,28 +223,44 @@ uint8_t getMaxBatteryValue(uint8_t maxTension){
 }
 
 /************************************************************************/
-/* Returns the battery tension                                          */
+/* Returns the battery usage percentage                                 */
 /************************************************************************/
 uint8_t getBatteryUsagePercentage(uint8_t adcValue, uint8_t maxTension, uint8_t minTension){
 	return (int)((((float)adcValue / (float)getMaxBatteryValue(maxTension) - ((float)minTension/(float)maxTension)) *100) /(100-(100*minTension/maxTension)) * 100) ;
 }
 
+/************************************************************************/
+/* Returns the real battery tension                                     */
+/************************************************************************/
 uint8_t getRealBatteryTension(uint8_t adcValue, uint8_t maxTension){
-	return  (int)((float)adcValue * 10 / (float)getMaxBatteryValue(maxTension) * maxTension ) ; // Pour plus de précision, il affiche la valeur fois 100
+	return  (int)((float)adcValue * 10 / (float)getMaxBatteryValue(maxTension) * maxTension ) ; // For more precision, output the value x10
+}
+
+/************************************************************************/
+/* This method protect the battery from undervoltage by 
+shutting down the engine at 6V                                          */
+/************************************************************************/
+void batteryProtection(){
+	mBatterie = getRealBatteryTension(adc_read(PA3), 9);
+	if(mBatterie < 63){
+		if(mBatterie < 60){
+			cutEngines = 1;
+			if(loopCount < 100){
+				PORTB = set_bit(PORTB, 4);
+				}else{
+				PORTB = clear_bit(PORTB, 4);
+				if(loopCount == 600){
+					loopCount = 0;
+				}
+			}
+			loopCount++;
+		}
+		else{
+			cutEngines = 0;
+			PORTB = set_bit(PORTB,4);
+		}
+		
+	}
 }
 
 
-void DelBattery(uint8_t battValue)
-{
-	if(battValue <= 100 && battValue > 80)
-	PORTB = 0b00011111;	
-	if(battValue <= 80 && battValue > 60)
-	PORTB = 0b00011111;
-	if(battValue <= 60 && battValue > 40)
-	PORTB = 0b00011111;
-	if(battValue <= 40 && battValue > 20)
-	PORTB = 0b00011111;
-	if(battValue <= 20 && battValue >= 0)
-	PORTB = 0b00011111;	
-	
-}
